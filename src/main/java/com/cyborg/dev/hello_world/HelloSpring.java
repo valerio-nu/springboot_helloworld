1c93e7172356eb39c8742b44f229ebba5ac7221f
Henry <henrichukwudi@gmail.com>
2022-10-09 16:06:10 +0100

    added thymeleaf and the html aspect

--- src/main/java/com/cyborg/dev/hello_world/HelloSpring.java
+++ src/main/java/com/cyborg/dev/hello_world/HelloSpring.java
@@ -1,14 +1,17 @@
 package com.cyborg.dev.hello_world;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloSpring {
-
+    String Message;
 
     @GetMapping("/")
-    public String GetHello(){
+    public String GetHello(Model model){
+        Message = "Hello World";
+        model.addAttribute("Message", Message);
 
         return "hello";
     }
